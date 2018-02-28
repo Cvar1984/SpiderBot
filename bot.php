cc7a0cdaa2301f05306ee8bcd30da9c1c8fc99d0
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2018-02-28 23:03:35 +0700

    Status Now using date()

--- bot.php
+++ bot.php
@@ -2,10 +2,10 @@
 <?php
 pilih:
 system('clear');
-$green = "\e[92m";
-$red   = "\e[91m";
-$yellow= "\e[93m";
-$blue  = "\e[36m";
+$green  = "\e[92m";
+$red    = "\e[91m";
+$yellow = "\e[93m";
+$blue   = "\e[36m";
 echo "\n$yellow
  ____        _     _           ____        _
 / ___| _ __ (_) __| | ___ _ __| __ )  ___ | |_
@@ -23,46 +23,51 @@ Date    : 28-02-2018\n";
 echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
 @header('Content-Type: text/html; charset=UTF-8');
 function input($echo) {
-echo "$echo --> ";
+    echo "$echo --> ";
 }
 input("Input Token From File / Paste? [y/n]");
-$pilih=trim(fgets(STDIN));
+$pilih = trim(fgets(STDIN));
 if($pilih == "y") {
-echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
-input("Filenane");
-$Fname=trim(fgets(STDIN));
-echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
-$token=file_get_contents($Fname);
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    input("Filenane");
+    $Fname = trim(fgets(STDIN));
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    $token = file_get_contents($Fname);
 } elseif($pilih == "n") {
-echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
-input("Token");
-$token=trim(fgets(STDIN));
-echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
-} else {	
-echo "$red(!)$yellow Invalid$red (!)\n";
-goto pilih;
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    input("Token");
+    $token = trim(fgets(STDIN));
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+} else {
+    echo "$red(!)$yellow Invalid$red (!)\n";
+    goto pilih;
 }
 input("Count");
-$count=trim(fgets(STDIN));
+$count = trim(fgets(STDIN));
 echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
 include('status.php');
-for($x=0;$x<$count;$x++) {
-$ch=curl_init();
-curl_setopt($ch, CURLOPT_URL,'https://graph.facebook.com/me/feed');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,"method=POST&message=$status[$x]&access_token=$token");
-curl_setopt($ch, CURLOPT_POST,1);
-$tampil=curl_exec($ch);
-curl_close($ch);
-echo "Send Status -> $status[$x]\n";
-echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
-echo "$tampil\n";
-echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
-$sleep=array("700","800","600","500"); // Detik
-$slp=array_rand($sleep);
-$slp2=$sleep[$slp];
-echo "Sleeping For $slp2 Second\n";
-echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
-sleep($slp2); // Random Delay
+for($x = 0; $x < $count; $x++) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/me/feed');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "method=POST&message=$status[$x]&access_token=$token");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    $tampil = curl_exec($ch);
+    curl_close($ch);
+    echo "Send Status -> $status[$x]\n";
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    echo "$tampil\n";
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    $sleep = array(
+        "700",
+        "800",
+        "600",
+        "500"
+    ); // Detik
+    $slp   = array_rand($sleep);
+    $slp2  = $sleep[$slp];
+    echo "Sleeping For $slp2 Second\n";
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    sleep($slp2); // Random Delay
 }
 ?>
--- status.php
+++ status.php
@@ -1,57 +1,58 @@
 <?php
-$status=array(
-"Kalau donlot file .iso pake command 'wget -c' biar pas putus ditengah jalan bisa nyambung lagi kayak hubungan kita.
-Powered Jembud Bot™",
-"Aku rela ko jadi Processor yg kepanasan, asalkan kmu yg jadi heatsink'y yg setiap saat bisa mendinginkan ku
-Powered Jembud Bot™",
-"Gak usah nyari celah xss deh, karena ketika kamu ngeklik hatiku udah muncul pop up namamu
-Powered Jembud Bot™",
-"jadi kamu deketin aku cuma mau 'cat /etc/passwd' -in aku? Terus kamu hek temen2 kostku, gitu?
-Powered Jembud Bot™",
-"berharap setelah aku berhasil login di hati kamu ga akan ada tombol logout, dan sessionku ga bakal pernah expired
-Powered Jembud Bot™",
-"Masa aku harus pake teknik symlink bypass buat buka2 folder hatimu yg open_basedir enabled.
-Powered Jembud Bot™",
-"Diriku dan Dirimu itu ibarat PHP dan MySQL yang belum terkoneksi :|
-Powered Jembud Bot™",
-"Jangan cuma bisa inject hatinya, tp harus bisa patchnya jg. Biar nggak selingkuh sama heker lain
-Powered Jembud Bot™",
-"Aku memang programmer PHP, tapi aku nggak akan php-in kamu kok.
-Powered Jembud Bot™",
-"Eneeeng. | Apache? | Km wanita yg paling Unix yg pernah aku kenal |
-Powered Jembud Bot™",
-"Sayang, capslock kamu nyala ya? | ngga, kenapa emangnya? | soalnya nama kamu ketulis gede bgt di hati aku | zzz! ( ‾_‾ )
-Powered Jembud Bot™",
-"Aku deketin kamu cuma untuk redirect ke hati temenmu.
-Powered Jembud Bot™",
-"Domain aja bisa parkir, masa cintaku ga bisa parkir dihatimu?
-Powered Jembud Bot™",
-"Aku boleh jadi pacarmu? | 400(Bad Request) | Aku cium boleh? | 401(Authorization Required) | Aku buka bajumu yah | 402(Payment Required)
-Powered Jembud Bot™",
-"kamu tau ga beda'y kamu sama sintax PHP,kalo sintax PHP itu susah di hafalin kalo kamu itu susah di lupain >_<",
-"Kamu dulu sekolah SMK ambil kejuruan apa? | Teknik Komputer Jaringan | Terus skrg bisa apa aja? | Menjaring hatimu lewat komputerku | :*
-Powered Jembud Bot™",
-"Jika cinta itu Array, maka, cintaku padamu tak pernah empty jika di unset().
-Powered Jembud Bot™",
-"SQLI ( Structured Query Love Injection )
-Powered Jembud Bot™",
-"aku ingin kamu rm -rf kan semua mantan di otak mu,aku lah root hati kamu.
-Powered Jembud Bot™",
-"senyumu bagaikan cooler yang menyejukan hatiku ketika sedang overclock.
-Powered Jembud Bot™",
-"kamu adalah terminalku, dimana aku menghabiskan waktuku untuk mengetikan beribu baris kode cinta untukmu :)
-Powered Jembud Bot™",
-"Aku seneng nongkrong di zone-h, karena disanalah aku arsipkan beberapa website yang ada foto kamunya.
-Powered Jembud Bot™",
-"hatiku ibarat vps hanya untukmu saja bukan shared hosting yg bisa tumpuk berbagai domain cinta.
-Powered Jembud Bot™",
-"Aku bukanlah VNC Server Tanpa Authentication yg bisa kamu pantau kapan saja.
-Powered Jembud Bot™",
-"Jangan men-dualboot-kan hatiku kepadamu.
-Powered Jembud Bot™",
-"KDE klh Cantiknya, GNOME klh Simplenya, FluxBox klh Ringannya, pokonya Semua DE itu Kalah Sama Kamu.
-Powered Jembud Bot™",
-"/mfspayload hati/gombal/bapakmu LHOST=L.0.v.3 LPORT=007 X > RasaCinta.exe
-Powered Jembud Bot"
+$date   = date('d-M-Y H:i');
+$status = array(
+    "Jika cinta itu Array, maka, cintaku padamu tak pernah empty jika di unset().
+--=(([DATE : $date]))=--",
+    "Aku rela ko jadi Processor yg kepanasan, asalkan kmu yg jadi heatsink'y yg setiap saat bisa mendinginkan ku
+--=(([DATE : $date]))=--",
+    "Gak usah nyari celah xss deh, karena ketika kamu ngeklik hatiku udah muncul pop up namamu
+--=(([DATE : $date]))=--",
+    "jadi kamu deketin aku cuma mau 'cat /etc/passwd' -in aku? Terus kamu hek temen2 kostku, gitu?
+--=(([DATE : $date]))=--",
+    "berharap setelah aku berhasil login di hati kamu ga akan ada tombol logout, dan sessionku ga bakal pernah expired
+--=(([DATE : $date]))=--",
+    "Masa aku harus pake teknik symlink bypass buat buka2 folder hatimu yg open_basedir enabled.
+--=(([DATE : $date]))=--",
+    "Diriku dan Dirimu itu ibarat PHP dan MySQL yang belum terkoneksi :|
+--=(([DATE : $date]))=--",
+    "Jangan cuma bisa inject hatinya, tp harus bisa patchnya jg. Biar nggak selingkuh sama heker lain
+--=(([DATE : $date]))=--",
+    "Aku memang programmer PHP, tapi aku nggak akan php-in kamu kok.
+--=(([DATE : $date]))=--",
+    "Eneeeng. | Apache? | Km wanita yg paling Unix yg pernah aku kenal |
+--=(([DATE : $date]))=--",
+    "Sayang, capslock kamu nyala ya? | ngga, kenapa emangnya? | soalnya nama kamu ketulis gede bgt di hati aku | zzz! ( ‾_‾ )
+--=(([DATE : $date]))=--",
+    "Aku deketin kamu cuma untuk redirect ke hati temenmu.
+--=(([DATE : $date]))=--",
+    "Domain aja bisa parkir, masa cintaku ga bisa parkir dihatimu?
+--=(([DATE : $date]))=--",
+    "Aku boleh jadi pacarmu? | 400(Bad Request) | Aku cium boleh? | 401(Authorization Required) | Aku buka bajumu yah | 402(Payment Required)
+--=(([DATE : $date]))=--",
+    "kamu tau ga beda'y kamu sama sintax PHP,kalo sintax PHP itu susah di hafalin kalo kamu itu susah di lupain >_<",
+    "Kamu dulu sekolah SMK ambil kejuruan apa? | Teknik Komputer Jaringan | Terus skrg bisa apa aja? | Menjaring hatimu lewat komputerku | :*
+--=(([DATE : $date]))=--",
+    "SQLI ( Structured Query Love Injection )
+--=(([DATE : $date]))=--",
+    "aku ingin kamu rm -rf kan semua mantan di otak mu,aku lah root hati kamu.
+--=(([DATE : $date]))=--",
+    "senyumu bagaikan cooler yang menyejukan hatiku ketika sedang overclock.
+--=(([DATE : $date]))=--",
+    "kamu adalah terminalku, dimana aku menghabiskan waktuku untuk mengetikan beribu baris kode cinta untukmu :)
+--=(([DATE : $date]))=--",
+    "Aku seneng nongkrong di zone-h, karena disanalah aku arsipkan beberapa website yang ada foto kamunya.
+--=(([DATE : $date]))=--",
+    "hatiku ibarat vps hanya untukmu saja bukan shared hosting yg bisa tumpuk berbagai domain cinta.
+--=(([DATE : $date]))=--",
+    "Aku bukanlah VNC Server Tanpa Authentication yg bisa kamu pantau kapan saja.
+--=(([DATE : $date]))=--",
+    "Jangan men-dualboot-kan hatiku kepadamu.
+--=(([DATE : $date]))=--",
+    "KDE klh Cantiknya, GNOME klh Simplenya, FluxBox klh Ringannya, pokonya Semua DE itu Kalah Sama Kamu.
+--=(([DATE : $date]))=--",
+    "/mfspayload hati/gombal/bapakmu LHOST=L.0.v.3 LPORT=007 X > RasaCinta.exe
+--=(([DATE : $date]))=--",
+    "Kalau donlot file .iso pake command 'wget -c' biar pas putus ditengah jalan bisa nyambung lagi kayak hubungan kita.
+--=(([DATE : $date]))=--"
 );
 ?>
