2161ee60c5b27b2ef0926ab171983f2e96518fbb
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2018-03-16 17:54:05 +0700

    Bot Telegram

--- files/status.php
+++ files/status.php
@@ -1,58 +1,59 @@
 <?php
-$date   = date('d-M-Y H:i');
+date_default_timezone_set('Asia/Jakarta');
+$date   = date('d-M-Y h:i:s');
 $status = array(
     "Jika cinta itu Array, maka, cintaku padamu tak pernah empty jika di unset().
---=(([DATE : $date]))=--",
+([ $date ])",
     "Aku rela ko jadi Processor yg kepanasan, asalkan kmu yg jadi heatsink'y yg setiap saat bisa mendinginkan ku
---=(([DATE : $date]))=--",
+([ $date ])",
     "Gak usah nyari celah xss deh, karena ketika kamu ngeklik hatiku udah muncul pop up namamu
---=(([DATE : $date]))=--",
+([ $date ])",
     "jadi kamu deketin aku cuma mau 'cat /etc/passwd' -in aku? Terus kamu hek temen2 kostku, gitu?
---=(([DATE : $date]))=--",
+([ $date ])",
     "berharap setelah aku berhasil login di hati kamu ga akan ada tombol logout, dan sessionku ga bakal pernah expired
---=(([DATE : $date]))=--",
+([ $date ])",
     "Masa aku harus pake teknik symlink bypass buat buka2 folder hatimu yg open_basedir enabled.
---=(([DATE : $date]))=--",
+([ $date ])",
     "Diriku dan Dirimu itu ibarat PHP dan MySQL yang belum terkoneksi :|
---=(([DATE : $date]))=--",
+([ $date ])",
     "Jangan cuma bisa inject hatinya, tp harus bisa patchnya jg. Biar nggak selingkuh sama heker lain
---=(([DATE : $date]))=--",
+([ $date ])",
     "Aku memang programmer PHP, tapi aku nggak akan php-in kamu kok.
---=(([DATE : $date]))=--",
+([ $date ])",
     "Eneeeng. | Apache? | Km wanita yg paling Unix yg pernah aku kenal |
---=(([DATE : $date]))=--",
+([ $date ])",
     "Sayang, capslock kamu nyala ya? | ngga, kenapa emangnya? | soalnya nama kamu ketulis gede bgt di hati aku | zzz! ( ‾_‾ )
---=(([DATE : $date]))=--",
+([ $date ])",
     "Aku deketin kamu cuma untuk redirect ke hati temenmu.
---=(([DATE : $date]))=--",
+([ $date ])",
     "Domain aja bisa parkir, masa cintaku ga bisa parkir dihatimu?
---=(([DATE : $date]))=--",
+([ $date ])",
     "Aku boleh jadi pacarmu? | 400(Bad Request) | Aku cium boleh? | 401(Authorization Required) | Aku buka bajumu yah | 402(Payment Required)
---=(([DATE : $date]))=--",
+([ $date ])",
     "kamu tau ga beda'y kamu sama sintax PHP,kalo sintax PHP itu susah di hafalin kalo kamu itu susah di lupain >_<",
     "Kamu dulu sekolah SMK ambil kejuruan apa? | Teknik Komputer Jaringan | Terus skrg bisa apa aja? | Menjaring hatimu lewat komputerku | :*
---=(([DATE : $date]))=--",
+([ $date ])",
     "SQLI ( Structured Query Love Injection )
---=(([DATE : $date]))=--",
+([ $date ])",
     "aku ingin kamu rm -rf kan semua mantan di otak mu,aku lah root hati kamu.
---=(([DATE : $date]))=--",
+([ $date ])",
     "senyumu bagaikan cooler yang menyejukan hatiku ketika sedang overclock.
---=(([DATE : $date]))=--",
+([ $date ])",
     "kamu adalah terminalku, dimana aku menghabiskan waktuku untuk mengetikan beribu baris kode cinta untukmu :)
---=(([DATE : $date]))=--",
+([ $date ])",
     "Aku seneng nongkrong di zone-h, karena disanalah aku arsipkan beberapa website yang ada foto kamunya.
---=(([DATE : $date]))=--",
+([ $date ])",
     "hatiku ibarat vps hanya untukmu saja bukan shared hosting yg bisa tumpuk berbagai domain cinta.
---=(([DATE : $date]))=--",
+([ $date ])",
     "Aku bukanlah VNC Server Tanpa Authentication yg bisa kamu pantau kapan saja.
---=(([DATE : $date]))=--",
+([ $date ])",
     "Jangan men-dualboot-kan hatiku kepadamu.
---=(([DATE : $date]))=--",
+([ $date ])",
     "KDE klh Cantiknya, GNOME klh Simplenya, FluxBox klh Ringannya, pokonya Semua DE itu Kalah Sama Kamu.
---=(([DATE : $date]))=--",
+([ $date ])",
     "/mfspayload hati/gombal/bapakmu LHOST=L.0.v.3 LPORT=007 X > RasaCinta.exe
---=(([DATE : $date]))=--",
+([ $date ])",
     "Kalau donlot file .iso pake command 'wget -c' biar pas putus ditengah jalan bisa nyambung lagi kayak hubungan kita.
---=(([DATE : $date]))=--"
+([ $date ])"
 );
 ?>
---
+++ telegram.php
@@ -0,0 +1,96 @@
+#!/data/data/com.termux/files/usr/bin/php
+<?php
+if(strtolower(substr(PHP_OS, 0, 3)) == "win") {
+$bersih="cls";
+} else {
+$bersih="clear";
+}
+system($bersih);
+$green  = "\e[92m";
+$red    = "\e[91m";
+$yellow = "\e[93m";
+$blue   = "\e[36m";
+echo "\n$yellow
+ ____        _     _           ____        _
+/ ___| _ __ (_) __| | ___ _ __| __ )  ___ | |_
+\___ \| '_ \| |/ _` |/ _ \ '__|  _ \ / _ \| __|
+ ___) | |_) | | (_| |  __/ |  | |_) | (_) | |_
+|____/| .__/|_|\__,_|\___|_|  |____/ \___/ \__|
+      |_|";
+echo "\n$blue
+Author  : Cvar1984
+Code    : PHP
+Github  : http://github.com/Cvar1984
+Team    : BlackHole Security
+Version : 3.1 ( Alpha )
+Date    : 15-03-2018\n";
+echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+function input($echo) {
+    echo "$echo --> ";
+}
+input("Chat Id");
+$cid=trim(fgets(STDIN));
+input("Quote / Text [q/t]");
+$tipe=trim(fgets(STDIN));
+echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+if($tipe == "q") {
+input("Count");
+$count = trim(fgets(STDIN));
+include("files/images.php");
+include("files/status.php");
+$n=count($jpg);
+$c=count($status);
+$back="false";
+$url="https://api.telegram.org/bot553163661:AAH2nNAfyM_G8tKUKgvyRPb0Kzg4eEloY2A/sendPhoto";
+}
+elseif($tipe == "t") {
+pesan:
+input("Text");
+$status=trim(fgets(STDIN));
+$url="https://api.telegram.org/bot553163661:AAH2nNAfyM_G8tKUKgvyRPb0Kzg4eEloY2A/sendMessage";
+$postdata="chat_id={$cid}&text={$status}";
+$count="1";
+$back="true";
+} else {
+die($yellow."Input Failed".$green);
+}
+for($x = 0; $x < $count; $x++) {
+	if($back == "false") {
+$postdata="chat_id={$cid}&photo={$jpg[$x%$n]}&caption={$status[$x%$c]}";
+}
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,$postdata);    
+    curl_setopt($ch, CURLOPT_POST, 1);
+    $tampil=curl_exec($ch);
+    curl_close($ch);
+    if($back == "true") {
+    echo "Send Status -> ".$status."\n";
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    } else {
+    echo "Send Status -> ".$status[$x]."\n";
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    }
+    if(preg_match("/true/", $tampil)) {
+    	echo "Status Updated\n";
+    	} else {
+    	die($yellow . "(!) Check Your Post Data (!)\n");
+    	}
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    if($back == "true") {
+    	goto pesan;
+    } else {
+    $sleep = array(
+        "120",
+        "200",
+        "300"
+    ); // Detik
+    $slp   = array_rand($sleep);
+    $slp2  = $sleep[$slp];
+    echo "Sleeping For $slp2 Second\n";
+    echo "$red=========================== Cvar1984 ))=====(@)>$green\n";
+    sleep($slp2); // Random Interval
+}
+}
+?>
